8909f33e33d4a649894f7393292452a14c4a5852
liuyangxing <liuyangxing@bytedance.com>
2025-04-29 22:13:18 +0800

    fix(group): undo/redo of create node or ungroup not work

--- apps/demo-free-layout/src/plugins/free-group-plugin/workflow-group-service.ts
+++ apps/demo-free-layout/src/plugins/free-group-plugin/workflow-group-service.ts
@@ -1,11 +1,15 @@
 import {
   FlowGroupService,
-  FreeOperationType,
+  FlowNodeBaseType,
   HistoryService,
   inject,
   injectable,
   nanoid,
+  TransformData,
+  WorkflowDocument,
   WorkflowNodeEntity,
+  WorkflowNodeJSON,
+  WorkflowOperationBaseService,
 } from '@flowgram.ai/free-layout-editor';
 
 import { WorkflowGroupUtils } from './utils';
@@ -13,6 +17,10 @@ import { WorkflowGroupUtils } from './utils';
 @injectable()
 /** 分组服务 */
 export class WorkflowGroupService extends FlowGroupService {
+  @inject(WorkflowDocument) private document: WorkflowDocument;
+
+  @inject(WorkflowOperationBaseService) freeOperationService: WorkflowOperationBaseService;
+
   @inject(HistoryService) private historyService: HistoryService;
 
   /** 创建分组节点 */
@@ -20,37 +28,57 @@ export class WorkflowGroupService extends FlowGroupService {
     if (!WorkflowGroupUtils.validate(nodes)) {
       return;
     }
-    const sortedNodes: WorkflowNodeEntity[] = nodes.sort((a, b) => a.index - b.index);
-    const fromNode = sortedNodes[0];
     const groupId = `group_${nanoid(5)}`;
-    this.historyService.pushOperation({
-      type: FreeOperationType.createGroup,
-      value: {
-        targetId: fromNode.id,
-        groupId,
-        nodeIds: nodes.map((node) => node.id),
+    const groupJSON: WorkflowNodeJSON = {
+      type: FlowNodeBaseType.GROUP,
+      id: groupId,
+      meta: {
+        position: {
+          x: 0,
+          y: 0,
+        },
       },
+    };
+    this.historyService.startTransaction();
+    this.document.createWorkflowNodeByType(
+      FlowNodeBaseType.GROUP,
+      {
+        x: 0,
+        y: 0,
+      },
+      groupJSON
+    );
+    nodes.forEach((node) => {
+      this.freeOperationService.moveNode(node, {
+        parent: groupId,
+      });
     });
+    this.historyService.endTransaction();
   }
 
   /** 取消分组 */
   public ungroup(groupNode: WorkflowNodeEntity): void {
-    const group = this.groupController(groupNode);
-    if (!group) {
+    const groupBlocks = groupNode.blocks.slice();
+    if (!groupNode.parent) {
       return;
     }
-    const nodes = group.nodes;
-    if (!groupNode.pre) {
-      return;
-    }
-    group.collapse();
-    this.historyService.pushOperation({
-      type: FreeOperationType.ungroup,
-      value: {
-        groupId: groupNode.id,
-        targetId: groupNode.pre.id,
-        nodeIds: nodes.map((node) => node.id),
-      },
+    const groupPosition = groupNode.transform.position;
+
+    this.historyService.startTransaction();
+    groupBlocks.forEach((node) => {
+      this.freeOperationService.moveNode(node, {
+        parent: groupNode.parent?.id,
+      });
     });
+    groupNode.dispose();
+    groupBlocks.forEach((node) => {
+      const transform = node.getData(TransformData);
+      const position = {
+        x: transform.position.x + groupPosition.x,
+        y: transform.position.y + groupPosition.y,
+      };
+      this.freeOperationService.updateNodePosition(node, position);
+    });
+    this.historyService.endTransaction();
   }
 }
