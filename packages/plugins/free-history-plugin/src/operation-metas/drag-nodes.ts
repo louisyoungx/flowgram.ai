f8ca093152c06fde19db04b24b2872f56d8e7f51
liuyangxing <liuyangxing@bytedance.com>
2025-05-07 20:55:06 +0800

    fix(ci): lock & ts-check & test errors

--- packages/plugins/free-history-plugin/src/operation-metas/drag-nodes.ts
+++ packages/plugins/free-history-plugin/src/operation-metas/drag-nodes.ts
@@ -1,6 +1,5 @@
 import { type OperationMeta } from '@flowgram.ai/history';
 import { WorkflowDocument } from '@flowgram.ai/free-layout-core';
-import { FlowNodeTransformData } from '@flowgram.ai/document';
 import { type PluginContext, TransformData } from '@flowgram.ai/core';
 
 import { type DragNodeOperationValue, FreeOperationType } from '../types';
