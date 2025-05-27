a16d8be0342864413507e8b727ff26e40dfad2e7
liuyangxing <liuyangxing@bytedance.com>
2025-05-27 12:32:42 +0800

    feat(runtime): init nodejs runtime

---
+++ packages/runtime/nodejs/.eslintrc.cjs
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@flowgram.ai/eslint-config');
+
+module.exports = defineConfig({
+  parser: '@typescript-eslint/parser',
+  preset: 'node',
+  packageRoot: __dirname,
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaVersion: 2017,
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true,
+    }
+  },
+  rules: {
+    'no-console': 'off',
+  },
+  plugins: ['json', '@typescript-eslint'],
+});
---
+++ packages/runtime/nodejs/src/router/routers/posts.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+import { TRPCError } from '@trpc/server';
+
+import { publicProcedure, router } from '../trpc';
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+interface Db {
+  posts: Post[];
+}
+
+const db: Db = {
+  posts: [],
+};
+
+export const postsRouter = router({
+  create: publicProcedure.input(z.object({ title: z.string() })).mutation(({ input, ctx }) => {
+    if (ctx.user.name !== 'nyan') {
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+    const id = db.posts.length + 1;
+    const post = { id, ...input };
+    db.posts.push(post);
+    return post;
+  }),
+  list: publicProcedure.query(() => db.posts),
+  reset: publicProcedure.mutation(() => {
+    db.posts = [];
+  }),
+});
