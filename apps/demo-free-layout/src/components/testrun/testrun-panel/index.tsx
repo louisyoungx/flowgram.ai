bc88a312c290203f253bce30643c786fd11655dd
liuyangxing <liuyangxing@bytedance.com>
2025-07-17 16:18:17 +0800

    fix(demo): should validate form before test run

--- apps/demo-free-layout/src/components/testrun/testrun-panel/index.tsx
+++ apps/demo-free-layout/src/components/testrun/testrun-panel/index.tsx
@@ -46,11 +46,9 @@ export const TestRunSidePanel: FC<TestRunSidePanelProps> = ({ visible, onCancel 
     }
     setResult(undefined);
     setErrors(undefined);
-    setRunning(true);
-    try {
-      await runtimeService.taskRun(values);
-    } catch (e: any) {
-      setErrors([e.message]);
+    const taskID = await runtimeService.taskRun(values);
+    if (taskID) {
+      setRunning(true);
     }
   };
 
